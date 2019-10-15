9855d58913cfa695fafaa9ce091b07d788a2b3fa
urube <50559400+urube@users.noreply.github.com>
2019-10-15 13:14:08 +1300

    Test file for  Kieran's refactored codes
    Kieran has different approach to his refactored codes then the comfirmed interface_diagram. So the test code had to be slightly different.

--- blackbox_testing_.py
+++ blackbox_testing_.py
@@ -1,100 +1,107 @@
 """
-    >>> import tkinter
-    >>> canvas = tkinter.Canvas(tkinter.Tk(), width=500, height=500)
-    >>> from drawer_kieran import DrawerKieran
-    >>> from parser_jerry import ParserJerry
-    >>> d = DrawerKieran(canvas)
-    >>> p = ParserJerry(d)
-    >>> p.parse(["P 2"])
+    >>> from DrawerKieran import Drawer
+    >>> from ParserJonathanV2 import Parser
+    >>> d = Drawer()
+    >>> p = Parser(d)
+    >>> p.parse('P 2')
     Selected pen 2
 
-    >>> p.parse(["X 250"])
+    >>> p.parse('X 250')
     GOTO X=250
 
-    >>> p.parse(["Y 250"])
-    GOTO Y=250
-
-    >>> p.parse(["D 0"])
-    pen down
-
-    >>> p.parse(["W 200"])
-    drawing line of length 200 at 270 degrees
-
-    >>> p.parse(["N 100"])
-    drawing line of length 100 at 0 degrees
-
-    >>> p.parse(["E 200"])
-    drawing line of length 200 at 90 degrees
-
-    >>> p.parse(["S 100"])
-    drawing line of length 100 at 180 degrees
-
-    >>> p.parse(["U 0"])
-    pen up
-
-    >>> from drawer_jack import DrawerJack
-    >>> from parser_dang import ParserDang
-    >>> d = DrawerJack(canvas)
-    >>> p = ParserDang(d)
-    >>> p.parse(["P 2"])
-    Selected pen 2
-
-    >>> p.parse(["X 250"])
+    >>> p.parse('Y 250')
     GOTO X=250
 
-    >>> p.parse(["Y 250"])
-    GOTO Y=250
-
-    >>> p.parse(["D 0"])
+    >>> p.parse('D 0')
     pen down
 
-    >>> p.parse(["W 200"])
+    >>> p.parse('W 200')
     drawing line of length 200 at 270 degrees
 
-    >>> p.parse(["N 100"])
+    >>> p.parse('N 100')
     drawing line of length 100 at 0 degrees
 
-    >>> p.parse(["E 200"])
+    >>> p.parse('E 200')
     drawing line of length 200 at 90 degrees
 
-    >>> p.parse(["S 100"])
+    >>> p.parse('S 100')
     drawing line of length 100 at 180 degrees
 
-    >>> p.parse(["U 0"])
+    >>> p.parse('L 100')
+    'L': Not a command
+
+    >>> p.parse('U 0')
     pen up
 
-    >>> from drawer_turtle_jack import DrawerTurtleJack
-    >>> from parser_jonathan_v2 import ParserJonathon
-    >>> d = DrawerTurtleJack(canvas)
-    >>> p = ParserJonathon(d)
-    >>> p.parse(["P 2"])
+    >>> from DrawerTurtleJack import Drawer
+    >>> from ParserJonathanV2 import Parser
+    >>> d = Drawer()
+    >>> p = Parser(d)
+    >>> p.parse('P 2')
     Selected pen 2
 
-    >>> p.parse(["X 250"])
+    >>> p.parse('X 250')
     pen up
     GOTO X=250
 
-    >>> p.parse(["Y 250"])
+    >>> p.parse('Y 250')
     pen up
-    GOTO Y=250
+    GOTO X=250
 
-    >>> p.parse(["D 0"])
+    >>> p.parse('D 0')
     pen down
 
-    >>> p.parse(["W 200"])
+    >>> p.parse('W 200')
     drawing line of length 200 at 270 degrees
 
-    >>> p.parse(["N 100"])
+    >>> p.parse('N 100')
     drawing line of length 100 at 0 degrees
 
-    >>> p.parse(["E 200"])
+    >>> p.parse('E 200')
     drawing line of length 200 at 90 degrees
 
-    >>> p.parse(["S 100"])
+    >>> p.parse('S 100')
     drawing line of length 100 at 180 degrees
 
-    >>> p.parse(["U 0"])
+    >>> p.parse('L 100')
+    'L': Not a command
+
+    >>> p.parse ('S 10000000000000000000000000000000000000000000000000')
+    drawing line of length 100 at 180 degrees
+
+    >>> p.parse('U 0')
     pen up
+
+    # >>> from drawer_turtle_jack import DrawerTurtleJack
+    # >>> from parser_jonathan_v2 import ParserJonathon
+    # >>> d = DrawerTurtleJack()
+    # >>> p = ParserJonathon(d)
+    # >>> p.parse('P 2')
+    # Selected pen 2
+    #
+    # >>> p.parse('X 250')
+    # GOTO X=250
+    #
+    # >>> p.parse('Y 250')
+    # GOTO X=250
+    #
+    # >>> p.parse('D 0')
+    # pen down
+    #
+    # >>> p.parse('W 200')
+    # drawing line of length 200 at 270 degrees
+    #
+    # >>> p.parse('N 100')
+    # drawing line of length 100 at 0 degrees
+    #
+    # >>> p.parse('E 200')
+    # drawing line of length 200 at 90 degrees
+    #
+    # >>> p.parse('S 100')
+    # drawing line of length 100 at 180 degrees
+    #
+    # >>> p.parse('U 0')
+    # pen up
 
 """
 import doctest
